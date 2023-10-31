c302e2e84f94c0d23b3adced4bd5740a2bdddabb
mohamed khan <mohamedkhan@mohameds-MBP.lan>
2023-10-30 23:48:02 -0400

    added a loading screen component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,16 @@
 import Navbar from "./components/Navbar/Navbar";
+import Loading from "./components/Loading/Loading";
+
+import { useEffect, useState } from "react";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    setTimeout(() => setLoading(false), 3300);
+  }, []);
+  if (loading) {
+    return <Loading />;
+  }
 
   return (
     <>
