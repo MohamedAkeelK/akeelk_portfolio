9c5bf671aa03ab0a22ce4d25b42fd7d7fdd5e8f3
mohamed khan <mohamedkhan@mohameds-MBP.lan>
2023-11-01 20:31:03 -0400

    added new lotti file

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Lottie from "lottie-react";
-import animationData from "../../assets/coder.json";
+import animationData from "../../assets/coder2.json";
 import "./Hero.css";
 
 export default function Hero() {
