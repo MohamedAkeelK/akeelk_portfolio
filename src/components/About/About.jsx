13ca28add1625fa86267086f6de02fe5cc9998bc
mohamed khan <mohamedkhan@mohameds-MBP.lan>
2023-11-04 20:30:42 -0400

    started about page

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -3,9 +3,19 @@ import "./About.css";
 
 export default function About() {
   return (
-    <div>
-      <h1>My Expertise</h1>
-      <h1>ABOURSgjksdn sdfsd s g s dgsd f s</h1>
+    <div className="about-page">
+      <h1 className="expertise-title">My Expertise</h1>
+      <div className="expertise-box-ctr">
+        <div className="expertise-box expertise-box-1">
+          <h1>Software Development</h1>
+        </div>
+        <div className="expertise-box expertise-box-2">
+          <h1>Software Development</h1>
+        </div>
+        <div className="expertise-box expertise-box-3">
+          <h1>Software Development</h1>
+        </div>
+      </div>
     </div>
   );
 }
